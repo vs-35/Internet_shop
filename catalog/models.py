61a0934bc110ef5379e8f256310f19302bd4b3e5
User <eco-64@mail.ru>
2023-08-14 22:34:44 +0500

    task completed

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name='id')
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.TextField(max_length=1000, verbose_name='Описание')
+    image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100,
+                              verbose_name='Изображение', **NULLABLE)
+    category = models.ForeignKey("Category", on_delete=models.CASCADE)
+    price = models.IntegerField(verbose_name='Цена')
+    date_creation = models.DateTimeField(max_length=150, verbose_name='Дата создания')
+    date_change = models.DateTimeField (max_length=150, verbose_name='Дата изменения')
+
+    def __str__(self):
+        return f'''
+        {self.name}, {self.description},
+        Категория: {self.category}
+        Цена: {self.price}
+        '''
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.TextField(max_length=1000, verbose_name='Описание')
+    created_at = models.TextField(max_length=150, verbose_name='Создание')
+
+    def __str__(self):
+        return f'{self.name} {self.description}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Internet_shop',# Название БД
+        'USER': 'postgres',# Пользователь для подключения
+        'PASSWORD': 'secret', # Пароль для этого пользователя
+        'HOST': '127.0.0.1', # Адрес, на котором развернут сервер БД
+        'PORT': 5432, # Порт, на котором работает сервер БД
     }
 }
 
@@ -123,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
