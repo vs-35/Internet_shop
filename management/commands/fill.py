94a4792b877916484d3f60e2060702762c8c8f0e
User <eco-64@mail.ru>
2023-08-15 00:32:55 +0500

    task completed

---
+++ management/commands/fill.py
@@ -0,0 +1,17 @@
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        products_list = [
+            {'name': 'Бочонок для меда', 'description': 'Материалы:липа, дерево.Объем: 1,0 кг','category: 'Посуда': '', 'price': '889 руб.'}
+            {'name': 'Ложка чайная', 'description': 'Набор 6 шт.', 'category': 'Столовые приборы', 'price': '420 руб.'}
+            {'name': 'Обеденный стол', 'description': 'Круглый, лофт', 'category': 'Мебель', 'price': '4000 руб.'}
+        ]
+
+        products_for_create =[]
+        for products_item in products_list:
+            products_for_create.append(Product(products_item))
+
+        Product.objects.bulk-create(products_for_create)
