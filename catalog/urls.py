4e217314655e8abfbefdcffaeb18709153f276e3
User <eco-64@mail.ru>
2023-08-17 20:37:27 +0500

    task completed

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from catalog.views import index, home
 
 urlpatterns = [
-    path('', index, name='index'),
     path('', home, name='home'),
+    path('contacts/', index, name='index'),
 ]
